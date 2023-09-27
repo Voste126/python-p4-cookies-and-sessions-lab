ea7fe4dc368f422ae6c4917faa4f1f0f19fb407a
Voste126 <steveaustine126@gmail.com>
2023-09-27 20:22:38 +0300

    how to work with session

--- server/app.py
+++ server/app.py
@@ -15,6 +15,11 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+#set an intial count to the play
+@app.before_request
+def before_request():
+    session.setdefault('page_views', 0)
+
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
@@ -22,13 +27,60 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    # Query all articles from the database
+    articles = Article.query.all()
 
-    pass
+    # Create a list to store article data
+    article_list = []
+
+    # Iterate through the articles and convert them to dictionaries
+    for article in articles:
+        article_data = {
+            'id': article.id,
+            'author': article.author,
+            'title': article.title,
+            'content': article.content,
+            'preview': article.preview,
+            'minutes_to_read': article.minutes_to_read,
+            'date': article.date.strftime('%Y-%m-%d %H:%M:%S')  # Convert date to string
+        }
+        article_list.append(article_data)
+
+    # Return the list of articles as JSON
+    return jsonify(article_list), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Check if the user has viewed more than 3 pages
+    if session['page_views'] >= 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
-    pass
+    # Increment the page_views counter
+    session['page_views'] += 1
+
+    # Fetch the article data from your Article model by ID
+    article = Article.query.get(id)
+
+    # Check if the article exists
+    if article is None:
+        return jsonify({'message': 'Article not found'}), 404
+
+    # Create a response object
+    response = make_response(jsonify({
+        'id': article.id,
+        'author': article.author,
+        'title': article.title,
+        'content': article.content,
+        'preview': article.preview,
+        'minutes_to_read': article.minutes_to_read,
+        'date': article.date.strftime('%Y-%m-%d %H:%M:%S')  # Convert date to string
+    }))
+
+    # You can set additional headers if needed
+     # Set the Content-Type header to indicate JSON response
+    response.headers['Content-Type'] = 'application/json'
+
+    return response, 200
 
 if __name__ == '__main__':
     app.run(port=5555)
